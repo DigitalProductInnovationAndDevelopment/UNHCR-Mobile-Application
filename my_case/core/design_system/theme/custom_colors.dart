acc28f7c242e1e8fd6e73ed74c99da9ce6be06c7
mehmetbesenk <mehmet.besenk@ruutapp.com>
2024-05-18 22:08:33 +0200

    feature: add core module

---
+++ my_case/core/design_system/theme/custom_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CColors {
+  static const primary = Color(0xFF3F51B5);
+  static const primaryVariant = Color(0xFF303F9F);
+  static const secondary = Color(0xFF00BCD4);
+}
---
+++ my_case/core/design_system/theme/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+class CustomThemes {
+  static ThemeData ligthTheme = FlexThemeData.light(
+    scheme: FlexScheme.deepPurple,
+  );
+
+  static ThemeData darkTheme = FlexThemeData.dark(
+    scheme: FlexScheme.deepPurple,
+  );
+}
